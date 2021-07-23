9ad593daa93eb884714328b201e3f34aba879a45
Roman Rakhlin <romanrakhlin@icloud.com>
2021-07-23 13:18:48 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from linked_list import LinkedList
+
+my_list = LinkedList()
+
+my_list.append(2)
+my_list.append(4)
+my_list.append(8)
+my_list.a ppend(16)
+
+my_list.show()
+
+my_list.reverse()
+
+my_list.show()
